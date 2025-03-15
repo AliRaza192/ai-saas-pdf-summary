7606686f9fb0108b1a20cfea192e56c3d18cf270
AliRaza192 <alirazaqasim192@gmail.com>
2025-03-15 16:53:34 +0500

    how it works section step

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import BgGradient from "@/components/common/bg-gradient";
+import DemoSection from "@/components/home/demo-section";
 import HeroSection from "@/components/home/hero-section";
+import HowItWorksSection from "@/components/home/how-it-works-section";
 import { Button } from "@/components/ui/button";
 
 
@@ -9,6 +11,8 @@ export default function Home() {
       <BgGradient />
       <div className="flex flex-col">
         <HeroSection />
+        <DemoSection />
+        <HowItWorksSection />
       </div>
     </div>
   );
