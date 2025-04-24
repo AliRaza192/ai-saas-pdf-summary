2f63ab7a5fa5b7934402b0d2b4650f3255a9c9e1
AliRaza <AliRaza192>
2025-04-24 20:29:05 +0500

    layout file changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,11 +12,10 @@ const fontSans = FontSans({
   weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-
-
 export const metadata: Metadata = {
   title: "Sommaire - AI-Powered PDF Summarization",
-  description: "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
+  description:
+    "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
 };
 
 export default function RootLayout({
@@ -27,17 +26,13 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${fontSans.variable} font-sans antialiased`}
-        >
+        <body className={`${fontSans.variable} font-sans antialiased`}>
           <div className="relative flex min-h-screen flex-col">
             <Header />
-            <main className="flex-1">
-              {children}
-            </main>
+            <main className="flex-1">{children}</main>
             <Footer />
           </div>
-          <Toaster/>
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
